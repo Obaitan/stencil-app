3efc85d3924fe23c8a6711fcd3efcaf5ff133078
Obaitan <elero.obaitan@gmail.com>
2021-10-31 22:42:42 +0100

    Created login module.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,24 @@
+from flask import render_template, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 
-@app.route("/")
-@app.route("/login")
+@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<h1>This is the login page</h1>"
+    if current_user.is_authenticated:
+        return redirect(url_for("dashboard"))
+    form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(
+            attempted_password=form.password.data
+        ):
+            login_user(attempted_user)
+            return redirect(url_for("dashboard"))
+        else:
+            flash(
+                f"Incorrect Username Or Password! Please Try Again.", category="danger"
+            )
+            return redirect(request.url)
+    return render_template("login.html", form=form)
